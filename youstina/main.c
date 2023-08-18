eb267ddc2a9a8e606bcf4952a36557895d5bf064
YoustinaHany01 <youstinahany89@gmail.com>
2023-08-19 02:45:30 +0300

    add project

---
+++ youstina/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDB.h"
+#include "STD.h"
+#include "SDB_APP.h"
+#include "linkedlist.h"
+
+
+
+int main(void)
+{ setbuf(stdout,NULL);
+
+
+
+	SDB_APP ();
+
+
+
+	return 0;
+}
