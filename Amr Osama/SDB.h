af664354cdfe0a7c6f13faf1b462b13480dca83e
Mohned98 <mohnedmohamed.98@hotmail.com>
2023-08-31 02:16:05 +0300

    Remove duplicated files

--- Amr Osama/SDB.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef SDB_H_
-#define SDB_H_
-
-	void SDB_APP();
-
-#endif /* SDB_H_ */
--- Sama/myproject.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "STD.h"
-#include "SDB.h"
-int main(void) {
-	setbuf(stdout,NULL);
-	SDB_APP();
-	return 0;
-}
